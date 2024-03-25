93eec3ae8693aac5f638f3efbf7bcfa102085f6f
Dhanielgeek <dhanielknight46@gmail.com>
2024-03-25 06:14:01 +0100

    products page done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
+import Products from './Products'
+import Product from '../Components/data.json'
+
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Products prod={Product}/>
     </div>
   )
 }
